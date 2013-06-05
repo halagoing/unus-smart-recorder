6f7b678d57f9a48b8bac1315f16c5c75dc68c54f
bond <bond@dogok-73-82.kaist.ac.kr>
2013-06-05 20:32:58 +0900

    modification SRDbHelper

--- src/com/unus/smartrecorder/SRDbHelper.java
+++ src/com/unus/smartrecorder/SRDbHelper.java
@@ -6,9 +6,45 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class SRDbHelper extends SQLiteOpenHelper{
 	
-	private static final String DB_NAME = SRConfig.DB_NAME;
-    private static final int DB_VERSION = SRConfig.DB_VERSION;
+	private static final String DB_NAME = "SMART_RECORDER";
+    private static final int DB_VERSION = 1;
 	
+    // voice table
+    private static final String TABLE_VOICE = "voice";
+    private static final String VOICE_COLUMN_VOICE_ID = "voice_id";
+    private static final String VOICE_COLUMN_CREATED_DATETIME = "created_datetime";
+    private static final String VOICE_COLUMN_VOICE_PATH = "voice_path";
+    private static final String VOICE_COLUMN_DOCUMENT_PATH = "document_path";
+    
+    private static final String VOICE_CREATE = "create table"
+    		+ TABLE_VOICE + "(" + VOICE_COLUMN_VOICE_ID
+    		+ " INTEGER primary key autoincrement, " + VOICE_COLUMN_CREATED_DATETIME
+    		+ " DATETIME default current_timestamp, " + VOICE_COLUMN_VOICE_PATH
+    		+ " TEXT not null, " + VOICE_COLUMN_DOCUMENT_PATH
+    		+ " TEXT);";
+    
+    // tag table
+    private static final String TABLE_TAG = "tag";
+    private static final String TAG_COLUMN_TAG_ID = "tag_id";
+    private static final String TAG_COLUMN_CREATED_DATETIME = "created_datetime";
+    private static final String TAG_COLUMN_VOICE_ID = "voice_id";
+    private static final String TAG_COLUMN_TYPE = "type";
+    private static final String TAG_COLUMN_CONTENT = "content";
+    private static final String TAG_COLUMN_TAG_TIME = "tag_time";
+    
+    private static final int TEXT_TAG_TYPE = 1;
+    private static final int PHOTO_TAG_TYPE = 2;
+    private static final int PAGE_TAG_TYPE = 3;
+    
+    private static final String TAG_CREATE = "create table"
+    		+ TABLE_TAG + "(" + TAG_COLUMN_TAG_ID
+    		+ " INTEGER primary key autoincrement, " + TAG_COLUMN_CREATED_DATETIME
+    		+ " DATETIME default current_timestamp, " + TAG_COLUMN_VOICE_ID
+    		+ " INTEGER not null, " + TAG_COLUMN_TYPE
+    		+ " INTEGER default "+ TEXT_TAG_TYPE +", " + TAG_COLUMN_CONTENT
+    		+ " TEXT not null, " + TAG_COLUMN_TAG_TIME
+    		+ " TEXT not null);";
+    
     public SRDbHelper(Context context) {
 		// TODO Auto-generated constructor stub
     	super(context, DB_NAME, null, DB_VERSION);
@@ -17,35 +53,15 @@ public class SRDbHelper extends SQLiteOpenHelper{
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
 		// create Table
-    	db.execSQL(getCreateVoiceTableQuery());
-    	db.execSQL(getCreateTagTableQuery());
-	}
-	
-	private String getCreateVoiceTableQuery() {
-		String query = "create table "+SRConfig.DB_VOICE_TABLE_NAME+"("+
-    	"voice_id INTEGER primary key autoincrement,"+
-    	"created_time DATETIME default current_timestamp,"+
-    	"voice_path TEXT not null,"+
-    	"document_path TEXT)";
-		return query;
-	}
-	
-	private String getCreateTagTableQuery() {
-		String query = "create table "+SRConfig.DB_TAG_TABLE_NAME+"("+
-    	"tag_id INTEGER primary key autoincrement,"+
-    	"created_time DATETIME default current_timestamp,"+
-    	"voice_id INTEGER not null,"+
-    	"type INTEGER not null,"+
-    	"content TEXT not null,"+
-    	"tag_time TEXT not null)";
-		return query;
+    	db.execSQL(VOICE_CREATE);
+    	db.execSQL(TAG_CREATE);
 	}
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
-		db.execSQL("drop table if exist voice");
-		db.execSQL("drop table if exist tag");
+		db.execSQL("DROP TABLE IF EXISTS" + TABLE_VOICE);
+		db.execSQL("DROP TABLE IF EXISTS" + TABLE_TAG);
 		onCreate(db);
 	}
 }
