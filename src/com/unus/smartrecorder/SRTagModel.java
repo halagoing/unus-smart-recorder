f26761fb7ea71e474bb9fd854d4078bff7622c24
bond <bond@dogok-73-82.kaist.ac.kr>
2013-06-05 20:36:59 +0900

    add Tag,Voice Model

---
+++ src/com/unus/smartrecorder/SRTagModel.java
@@ -0,0 +1,53 @@
+package com.unus.smartrecorder;
+
+public class SRTagModel {
+	private long tag_id;
+	private String created_datetime;
+	private long voice_id;
+	private int type;
+	private String content;
+	private String tag_time;
+	public long getTag_id() {
+		return tag_id;
+	}
+	public void setTag_id(long tag_id) {
+		this.tag_id = tag_id;
+	}
+	public String getCreated_datetime() {
+		return created_datetime;
+	}
+	public void setCreated_datetime(String created_datetime) {
+		this.created_datetime = created_datetime;
+	}
+	public long getVoice_id() {
+		return voice_id;
+	}
+	public void setVoice_id(long voice_id) {
+		this.voice_id = voice_id;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getTag_time() {
+		return tag_time;
+	}
+	public void setTag_time(String tag_time) {
+		this.tag_time = tag_time;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return content;
+	}
+	
+}
---
+++ src/com/unus/smartrecorder/SRVoiceModel.java
@@ -0,0 +1,40 @@
+package com.unus.smartrecorder;
+
+public class SRVoiceModel {
+	private long voice_id;
+	private String created_datetime;
+	private String voice_path;
+	private String document_path;
+	
+	public long getVoice_id() {
+		return voice_id;
+	}
+	public void setVoice_id(long voice_id) {
+		this.voice_id = voice_id;
+	}
+	public String getCreated_datetime() {
+		return created_datetime;
+	}
+	public void setCreated_datetime(String created_datetime) {
+		this.created_datetime = created_datetime;
+	}
+	public String getVoice_path() {
+		return voice_path;
+	}
+	public void setVoice_path(String voice_path) {
+		this.voice_path = voice_path;
+	}
+	public String getDocument_path() {
+		return document_path;
+	}
+	public void setDocument_path(String document_path) {
+		this.document_path = document_path;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return voice_path;
+	}
+	
+}
