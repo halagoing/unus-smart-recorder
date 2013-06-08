041737f2eb0ccbd0251c07568b5f91234607f977
Suhwan Hwang <entercritical@gmail.com>
2013-06-08 16:06:50 +0900

    녹음시 폴더 없음 경우 생성, 저장 경로 변경 (/sdcard/SmartRecorder

--- src/com/unus/smartrecorder/SRConfig.java
+++ src/com/unus/smartrecorder/SRConfig.java
@@ -6,7 +6,7 @@ public class SRConfig {
     
     
     //recorder
-    static final String AUDIO_RECORDER_FOLDER = "UNUS_RECORDER";
+    static final String AUDIO_RECORDER_FOLDER = "SmartRecorder";
     static final String VOICE_PATH_KEY = "voice_path_key";
     
     // database
