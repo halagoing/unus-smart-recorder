51b84207295d1f3e87421555d1362bece0b74499
Suhwan Hwang <entercritical@gmail.com>
2013-06-11 16:44:01 +0900

    녹음/재생 화면 리사이징 문제 수정
    녹음 버튼을 눌렀을때 키보드가 올라오면서 화면이 리사이징되서 하단 버튼이 올라오는 현상 수

--- src/com/unus/smartrecorder/SRVoiceController.java
+++ src/com/unus/smartrecorder/SRVoiceController.java
@@ -32,6 +32,7 @@ import android.view.MenuItem;
 import android.view.MenuItem.OnActionExpandListener;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -183,6 +184,8 @@ public class SRVoiceController implements SRVoiceControllerInterface {
             if (mActionBarShareItem != null)
                 mActionBarShareItem.setVisible(false);
             
+            // 201306111 Suhwan Hwang: Keyboard가 출력되었을때 화면이 리사이징되면서 하단 버튼이 올라가는 현상 수정
+            mActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
         } else if (SRVoice.PLAYER_MODE == mode) {
             if (isInit)
                 mSRVoiceView.setVoiceViewMode(SRVoice.PLAYER_MODE);
@@ -195,7 +198,9 @@ public class SRVoiceController implements SRVoiceControllerInterface {
                 mActionBarSearchItem.setVisible(true);
             if (mActionBarShareItem != null)
                 mActionBarShareItem.setVisible(true);
-            
+
+            // 201306111 Suhwan Hwang: Keyboard가 출력되었을때 화면이 리사이징되면서 하단 버튼이 올라가는 현상 수정
+            mActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
         } else if (SRVoice.SEARCH_MODE == mode) {
             mSRSearchView.setSearchViewMode();
             mActivity.setContentView(mSRSearchView);
@@ -207,6 +212,9 @@ public class SRVoiceController implements SRVoiceControllerInterface {
                 mActionBarSearchItem.setVisible(true);
             if (mActionBarShareItem != null)
                 mActionBarShareItem.setVisible(false);
+            
+            // 201306111 Suhwan Hwang: Keyboard가 출력되었을때 화면이 리사이징되면서 하단 버튼이 올라가는 현상 수정
+            mActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         }
     }    
     
